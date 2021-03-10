8022ad5055de684fcf9b45e5b62a44131b36ed19
Cade <phillipscb@hendrix.edu>
2021-03-09 21:18:20 -0600

    Designed central hub, added trigger to take you to first level, added new jellyfish animation

---
+++ Lab4/Assets/scripts/Level1.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            StartCoroutine(LoadYourAsyncScene("level1"));
+        }
+
+    }
+
+    IEnumerator LoadYourAsyncScene(string scene)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
---
+++ Lab4/Assets/scripts/sources.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://opengameart.org/content/anglerfish
+//https://scut.itch.io/7drl-tileset-2018?download
+//https://opengameart.org/content/primary-jellies
+public class sources : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
