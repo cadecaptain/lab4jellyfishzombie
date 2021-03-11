4aacad983a7bde0ea1498817c3810a01521316da
Cade <phillipscb@hendrix.edu>
2021-03-11 16:20:51 -0600

    added travel to final level

---
+++ Lab4/Assets/scripts/finallevel.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class finallevel : MonoBehaviour
+{
+    public Vector3 whereTo;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameManager.Instance.JellyNo();
+            GameManager.Instance.NextScene("finallevel", whereTo);
+
+        }
+
+    }
+
+
+}
