63af40bc15332a640cb371cdeee02eda16642fab
Matthew Kalahiki <kalahikimw@hendrix.edu>
2021-03-11 22:21:22 -0600

    canvas

--- Lab4/Assets/scripts/sources.cs
+++ Lab4/Assets/scripts/sources.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 // https://opengameart.org/content/anglerfish
 //https://scut.itch.io/7drl-tileset-2018?download
 //https://opengameart.org/content/primary-jellies
+
+/*music:https://freesound.org/people/soundmatch24/sounds/186876/
+ * zap:https://freesound.org/people/michael_grinnell/sounds/512471/
+ * projectile fire:https://freesound.org/people/TRNGLE/sounds/367960/
+ * swimming:https://freesound.org/people/Robinhood76/sounds/317067/
+ * bubbles:https://freesound.org/people/MattJ99/sounds/66787/
+ * */
 public class sources : MonoBehaviour
 {
     // Start is called before the first frame update
